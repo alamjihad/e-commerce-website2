61219eb321f880006201702f8491a9bf02bdff62
Jihad <bdjihad1862@gmail.com>
2023-09-06 20:27:14 +0600

    modified homepaqge

--- Homepage/main.js
+++ Homepage/main.js
@@ -20,4 +20,30 @@ if(bar){
   bar.addEventListener('click',()=>{
     nav.classList.add('active');
   })
+};
+
+
+
+
+/*var indexValue = 1;
+showImg(indexValue);
+function side_slide(e){
+  showImg(indexValue +=e);
 }
+function showImg(e){
+  var i;
+  const img = document.querySelectorAll('img');
+  if(e>img.length)
+  {
+    indexValue = 1
+  }
+  if(e<1)
+  {
+    indexValue= img.length
+  }
+  for(i=0;i<img.length;i++)
+  {
+    img[i].style.display = "none";
+  }
+  img[indexValue - 1].style.display = "block";
+}*/
